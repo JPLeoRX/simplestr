ce71999cc39efa18ea1f886aee41771d34b21d55
leo <leo.ertuna@gmail.com>
2021-07-09 04:03:03 +0300

    Updated version and description

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r") as readme_file:
 
 setup_args = dict(
     name='simplestr',
-    version='0.3',
-    description="Simple annotations to automatically generate __str__(self) and __repr__(self) methods in classes",
-    keywords=['str', 'repr', 'generate', 'automatic', 'annotation'],
+    version='0.4',
+    description="Simple annotations to automatically generate __str__(self), __repr__(self) and __eq__(self, other) methods in classes",
+    keywords=['str', 'repr', 'eq', 'generate', 'automatic', 'annotation'],
     long_description=readme_text,
     long_description_content_type="text/markdown",
     license='MIT',
